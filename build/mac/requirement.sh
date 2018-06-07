5fa60e5fd6f435c580482d48fbcdb53420d04e9d
François Gindraud <francois.gindraud@univ-lyon1.fr>
2018-06-07 11:22:25 +0200

    Travis fix for mac 2

--- build/mac/requirement.sh
+++ build/mac/requirement.sh
@@ -2,9 +2,9 @@
 set -xue
 
 # Install qt5 and poppler from brew
-# brew update (disabled for test)
 brew install qt5
-brew install poppler --with-qt5
+brew install 'python@2' || brew link --overwrite 'python@2' # Python 2 install fails on the link step, failing the whole build. Dep of poppler.
+brew install poppler --with-qt
 
 # Add path to find qmake (that will handle all other paths)
 export PATH="$(brew --prefix qt5)/bin:${PATH}"
